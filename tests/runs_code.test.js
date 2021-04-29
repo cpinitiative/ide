79e4adfa9de30e38a51f43b1a5e3c7b8239293cc
Nathan Wang <nathan.r.wang@gmail.com>
2021-04-28 22:39:57 -0700

    remove test console.logs

--- tests/runs_code.test.js
+++ tests/runs_code.test.js
@@ -1,7 +1,5 @@
 test('should work', async () => {
-  console.log('running');
   await page.goto('http://localhost:3000/');
-  console.log('running');
   await page.waitForSelector('button:has-text("Run Code")');
   expect(page.url()).toMatch(/http:\/\/localhost:3000\/[A-z0-9_-]{19}/);
 
@@ -9,7 +7,6 @@ test('should work', async () => {
   expect(await page.$('[data-test-id="run-code-loading"]')).toBeTruthy();
   await page.waitForSelector('button:has-text("Run Code")');
   expect(await page.$('text=Accepted')).toBeTruthy();
-  console.log('running');
 
   await page.click('[data-test-id="input-editor"]');
   await page.keyboard.type('1 2 3');
@@ -20,7 +17,6 @@ test('should work', async () => {
   await page.waitForSelector('button:has-text("Run Code")');
   expect(await page.$('text=Accepted')).toBeTruthy();
   expect(await page.$('text="sum is 6"')).toBeTruthy();
-  console.log('running');
 
   await page.click('text=Main.py');
   await page.click('button:has-text("Run Code")');
@@ -28,7 +24,6 @@ test('should work', async () => {
   await page.waitForSelector('button:has-text("Run Code")');
   expect(await page.$('text=Accepted')).toBeTruthy();
   expect(await page.$('text="sum is 6"')).toBeTruthy();
-  console.log('running');
 
   await page.click('text=Main.cpp');
   await page.click('button:has-text("Run Code")');
@@ -36,5 +31,4 @@ test('should work', async () => {
   await page.waitForSelector('button:has-text("Run Code")');
   expect(await page.$('text=Accepted')).toBeTruthy();
   expect(await page.$('text="sum is 6"')).toBeTruthy();
-  console.log('running');
 });
