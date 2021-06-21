3f2bd7e186303deeb961f61aa38058ad1286fbfe
maggie-j-liu <maggie.j.liu@gmail.com>
2021-06-21 12:55:39 -0700

    decrease font size in usaco results

--- src/components/JudgeInterface/USACOResults.tsx
+++ src/components/JudgeInterface/USACOResults.tsx
@@ -19,12 +19,12 @@ const USACOTestCase = ({ data }: { data: any }) => {
         {data.symbol}
       </div>
       <span
-        className={`absolute bottom-0 left-[4px] ${textColor} text-sm font-bold`}
+        className={`absolute bottom-0 left-[4px] ${textColor} text-[0.8rem] font-bold`}
       >
         {data.trialNum}
       </span>
       <span
-        className={`absolute bottom-0 right-0 text-right ${textColor} leading-3 p-[2px] text-sm`}
+        className={`absolute bottom-0 right-0 text-right ${textColor} leading-3 p-[2px] text-[0.65rem]`}
       >
         {data.memory}
         <br />
