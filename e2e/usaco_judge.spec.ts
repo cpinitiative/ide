caf8ea0b674db5cf05ff89193062e0b605389066
Nathan Wang <nathan.r.wang@gmail.com>
2022-06-19 21:39:46 -0700

    fix usaco test

--- e2e/usaco_judge.spec.ts
+++ e2e/usaco_judge.spec.ts
@@ -50,21 +50,15 @@ test.describe('USACO Judge Functionality', () => {
     await page.locator('.view-lines').first().click();
 
     await page.locator('[data-test-id="code-editor"]').press('Control+a');
-    await page.locator('[data-test-id="code-editor"]')
-      .type(`import java.io.BufferedReader;
+    await page.locator('[data-test-id="code-editor"]').type(
+      `import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.StringTokenizer;
 public class AcowdemiaI {
-static int hIndex(Integer[] papers) {
-int h = papers.length;
-while (h > 0 && papers[h - 1] < h) {
-h--;
-}
-return h;
-}
+static int hIndex(Integer[] papers) {int h = papers.length;while (h > 0 && papers[h - 1] < h) h--;return h;}
 public static void main(String[] args) throws IOException {
 BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 StringTokenizer tokenizer = new StringTokenizer(in.readLine());
@@ -72,20 +66,16 @@ int n = Integer.parseInt(tokenizer.nextToken());
 int l = Integer.parseInt(tokenizer.nextToken());
 Integer[] papers = new Integer[n];
 tokenizer = new StringTokenizer(in.readLine());
-for (int j = 0; j < n; j++) {
-papers[j] = Integer.parseInt(tokenizer.nextToken());
-}
+for (int j = 0; j < n; j++) papers[j] = Integer.parseInt(tokenizer.nextToken());
 Arrays.sort(papers, Comparator.reverseOrder());
 int h = hIndex(papers);
-if (h != n) {
-for (int j = h; j >= 0 && j > h - l; j--) {
-papers[j]++;
-}
-}
+if (h != n)for (int j = h; j >= 0 && j > h - l; j--)papers[j]++;
 Arrays.sort(papers, Comparator.reverseOrder());
 System.out.println(hIndex(papers));
 }
-}`);
+}
+`.replace(/\n/g, '') // without this, sometimes monaco adds closing braces that causes test to fail
+    );
 
     await page.locator('button:has-text("Run Samples")').click();
 
