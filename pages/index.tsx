b8c47b10281bf8b2559ad30de430349e408ea458
Nathan Wang <nathan.r.wang@gmail.com>
2022-12-01 20:00:33 -0500

    feat: add notice about classrooms deprecation

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { useAtomValue } from 'jotai/utils';
 import { firebaseUserAtom } from '../src/atoms/firebaseUserAtoms';
 import { isUserSettingsLoadingAtom } from '../src/atoms/userSettings';
 import Dashboard from '../src/components/Dashboard/Dashboard';
+import { ExclamationTriangleIcon } from '@heroicons/react/20/solid';
 
 export default function DashboardPage(): JSX.Element {
   const firebaseUser = useAtomValue(firebaseUserAtom);
@@ -21,7 +22,40 @@ export default function DashboardPage(): JSX.Element {
           Real-Time Collaborative Online IDE
         </h1>
 
-        <div className="h-6"></div>
+        <div className="my-6">
+          <div className="rounded-md bg-yellow-800/25 p-4 max-w-xl">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <ExclamationTriangleIcon
+                  className="h-5 w-5 text-yellow-600"
+                  aria-hidden="true"
+                />
+              </div>
+              <div className="ml-3">
+                <h3 className="text-sm font-medium text-yellow-400">
+                  Classrooms Feature is Leaving
+                </h3>
+                <div className="mt-2 text-sm text-yellow-500">
+                  <p>
+                    Classrooms will no longer be accessible sometime soon.
+                    Please back up any code you need from classrooms locally. If
+                    you still need this feature, please email us at{' '}
+                    <a
+                      href="mailto:contact@joincpi.org"
+                      className="text-yellow-400 underline"
+                      target="_blank"
+                    >
+                      contact@joincpi.org
+                    </a>
+                    .
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="h-6"></div> */}
 
         {!firebaseUser || isUserSettingsLoading ? (
           <div className="text-gray-400 mt-6">Loading...</div>
