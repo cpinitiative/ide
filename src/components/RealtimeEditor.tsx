816ee9eb8f80494863a72764b96df1329f288fbe
Nathan Wang <nathan.r.wang@gmail.com>
2023-03-03 20:53:24 -0500

    use NODE_ENV to determine websocket server URL

--- src/components/RealtimeEditor.tsx
+++ src/components/RealtimeEditor.tsx
@@ -22,7 +22,7 @@ export interface RealtimeEditorProps extends EditorProps {
 }
 
 const WEBSOCKET_SERVER =
-  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+  process.env.NODE_ENV === 'development'
     ? 'ws://localhost:1234'
     : 'wss://yjs.usaco.guide:443';
 
