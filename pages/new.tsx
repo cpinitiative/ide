5a93e6a72da1ab733b0fd8553b1631f288a8f8d3
Nathan Wang <nathan.r.wang@gmail.com>
2023-03-17 03:28:54 -0400

    create new file functionality

--- pages/new.tsx
+++ pages/new.tsx
@@ -14,42 +14,60 @@ function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(' ');
 }
 
+const DEFAULT_COMPILER_OPTIONS = {
+  cpp: '-std=c++17 -O2 -Wall -Wextra -Wshadow -Wconversion -Wfloat-equal -Wduplicated-cond -Wlogical-op',
+  java: '',
+  py: '',
+};
+
 export default function NewFilePage() {
   const { userData, firebaseUser } = useNullableUserContext();
   const router = useRouter();
   const [lang, setLang] = useState<Language>('cpp');
+  const [fileName, setFileName] = useState('');
+  const [compilerOptions, setCompilerOptions] = useState('');
 
-  // const alreadyCreatedFile = useRef<boolean>(false);
+  const isPageLoading = !router.isReady || !userData || !firebaseUser;
 
-  // useEffect(() => {
-  //   if (userData && firebaseUser && !alreadyCreatedFile.current) {
-  //     alreadyCreatedFile.current = true;
-  //     (async () => {
-  //       const resp = await fetch(`/api/createNewFile`, {
-  //         method: 'POST',
-  //         headers: {
-  //           'Content-Type': 'application/json',
-  //         },
-  //         body: JSON.stringify({
-  //           workspaceName: 'Unnamed Workspace',
-  //           userID: firebaseUser.uid,
-  //           userName: firebaseUser.displayName, // TODO TEST
-  //           defaultPermission: userData.defaultPermission,
-  //         }),
-  //       });
-  //       const data = await resp.json();
-  //       if (resp.ok) {
-  //         router.push(`/${data.fileID.substring(1)}`);
-  //       } else {
-  //         alert('Error: ' + data.message);
-  //       }
-  //     })();
-  //   }
-  // }, [userData, firebaseUser]);
+  useEffect(() => {
+    setCompilerOptions(DEFAULT_COMPILER_OPTIONS[lang]);
+  }, [lang]);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isPageLoading) {
+      alert('Page is still loading, please try again later');
+      return;
+    }
+    if (!fileName) {
+      alert('Please enter a file name.');
+      return;
+    }
+    (async () => {
+      const resp = await fetch(`/api/createNewFile`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          workspaceName: fileName,
+          userID: firebaseUser.uid,
+          userName: firebaseUser.displayName,
+          defaultPermission: userData.defaultPermission,
+        }),
+      });
+      const data = await resp.json();
+      if (resp.ok) {
+        router.push(`/${data.fileID.substring(1)}`);
+      } else {
+        alert('Error: ' + data.message);
+      }
+    })();
+  };
 
   return (
     <div className="p-4 sm:p-6 md:p-8 lg:p-12 min-h-full flex flex-col max-w-6xl mx-auto">
-      <form className="max-w-md space-y-6 sm:space-y-8">
+      <form className="space-y-6 sm:space-y-8" onSubmit={handleSubmit}>
         <h1 className="text-white font-semibold md:text-xl xl:text-2xl">
           Create New File
         </h1>
@@ -67,7 +85,9 @@ export default function NewFilePage() {
                 type="text"
                 name="filename"
                 id="filename"
-                className="block w-full rounded-md border-0 py-1.5 bg-gray-900 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-500 focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-indigo-700 sm:text-sm sm:leading-6"
+                value={fileName}
+                onChange={e => setFileName(e.target.value)}
+                className="block w-full max-w-md rounded-md border-0 py-1.5 bg-gray-900 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-500 focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-indigo-700 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -88,7 +108,9 @@ export default function NewFilePage() {
                     className={({ active, checked }) =>
                       classNames(
                         'cursor-pointer focus-visible:outline-none',
-                        active ? 'ring-2 ring-indigo-800 ring-offset-2' : '',
+                        active
+                          ? 'focus-visible:ring-2 focus-visible:ring-indigo-800 focus-visible:ring-offset-2'
+                          : '',
                         checked
                           ? 'bg-indigo-800 text-white hover:bg-indigo-700'
                           : 'ring-1 ring-inset ring-gray-700 bg-gray-900 text-gray-100 hover:bg-gray-800',
@@ -116,6 +138,8 @@ export default function NewFilePage() {
                 type="text"
                 name="compilerOptions"
                 id="compilerOptions"
+                value={compilerOptions}
+                onChange={e => setCompilerOptions(e.target.value)}
                 className="block w-full rounded-md border-0 py-1.5 bg-gray-900 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-500 focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-indigo-700 sm:text-sm sm:leading-6 font-mono"
               />
             </div>
@@ -124,7 +148,8 @@ export default function NewFilePage() {
 
         <div className="space-x-4">
           <button
-            type="button"
+            type="submit"
+            disabled={isPageLoading}
             className="rounded-md bg-indigo-700 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Create File
