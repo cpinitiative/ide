f54774b0008d431bd301a2122379196c5e1b6ef2
Nathan Wang <nathan.r.wang@gmail.com>
2023-12-18 17:45:09 -0500

    get yjs working with codemirror

--- src/components/editor/CodemirrorEditor/CodemirrorEditor.tsx
+++ src/components/editor/CodemirrorEditor/CodemirrorEditor.tsx
@@ -3,22 +3,45 @@
 import ReactCodeMirror from '@uiw/react-codemirror';
 import { vscodeDark } from '@uiw/codemirror-theme-vscode';
 import { githubLight } from '@uiw/codemirror-theme-github';
-import { StreamLanguage } from '@codemirror/language';
 import { cpp } from '@codemirror/lang-cpp';
 import { EditorProps } from '../MonacoEditor/monaco-editor-types';
+import * as Y from 'yjs';
 
 import './codemirror-styles.css';
+import { useEffect, useMemo, useState } from 'react';
+import { yCollab } from 'y-codemirror.next';
 
 export const CodemirrorEditor = (props: EditorProps): JSX.Element => {
-  // props.yjsInfo!
+  const [yCollabExtension, setYCollabExtension] = useState<any | null>(null);
+  useEffect(() => {
+    if (!props.yjsInfo) return;
+
+    const undoManager = new Y.UndoManager(props.yjsInfo.yjsText);
+    setYCollabExtension(
+      yCollab(props.yjsInfo.yjsText, props.yjsInfo.yjsAwareness, {
+        undoManager,
+      })
+    );
+  }, [props.yjsInfo]);
+
+  const extensions = useMemo(() => {
+    if (!yCollabExtension) return [cpp()];
+    return [cpp(), yCollabExtension];
+  }, [yCollabExtension]);
+
   return (
     <ReactCodeMirror
-      value={props.value ?? undefined}
+      // force entire component to re-mount (and re-initialize codemirror) when yjs document ID changes
+      key={props.yjsInfo?.yjsText.doc.guid}
+      // we need to pass in props.yjsInfo.yjsText as a possible value here, since
+      // the yCollab() extension expects the value to be initialized to yText.toString()
+      // I don't think this needs to be re-computed every time though (only when extensions changes), but whatever
+      value={props.value || props.yjsInfo?.yjsText.toString() || undefined}
       theme={props.theme === 'light' ? githubLight : vscodeDark}
       height="100%"
       style={{ fontSize: '13px' }}
       readOnly={props.options?.readOnly ?? false}
-      // extensions={[StreamLanguage.define(cpp)]}
+      extensions={extensions}
     />
   );
 };
