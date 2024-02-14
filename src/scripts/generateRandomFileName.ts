b1273832973528634290f7598ca1e75603271aa1
aqzp <72946059+AsynchronousAI@users.noreply.github.com>
2024-02-14 12:46:47 -0600

    Update generateRandomFileName.ts (#135)

--- src/scripts/generateRandomFileName.ts
+++ src/scripts/generateRandomFileName.ts
@@ -77,7 +77,7 @@ export default function generateRandomFileName() {
   const randomAdjective =
     adjectives[Math.floor(Math.random() * adjectives.length)];
   const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
-  const randomNumber = Math.floor(Math.random() * 100);
+  const randomNumber = Math.floor(Math.random() * 1000);
 
   return `${randomAdjective}-${randomNoun}-${randomNumber}`;
 }
