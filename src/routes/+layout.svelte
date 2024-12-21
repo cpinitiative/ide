400503f5b6e3ec3cb45e764c0a1db7011e0614b5
Nathan Wang <nathan.r.wang@gmail.com>
2024-12-21 14:05:06 -0800

    add firebase auth

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { initFirebaseAuth } from '$lib/firebase/firebase.svelte';
 	import '../app.css';
+
 	let { children } = $props();
+
+	onMount(() => {
+		return initFirebaseAuth();
+	});
 </script>
 
 {@render children()}
