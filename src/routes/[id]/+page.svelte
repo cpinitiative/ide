e80f282e926fee6056672eac6bd23a211f5f2d12
Nathan Wang <nathan.r.wang@gmail.com>
2024-12-21 20:24:21 -0800

    add monaco editor

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,24 +1,36 @@
 <script lang="ts">
+	import MonacoEditor, { layoutEditors } from '$lib/components/MonacoEditor.svelte';
+	import { onMount } from 'svelte';
 	import Layout from './components/Layout.svelte';
 
 	let { data } = $props();
 	let fileId = data.fileId;
+
+  // When the window resizes, we should resize the monaco editors
+	onMount(() => {
+		function handleResize() {
+			layoutEditors();
+		}
+
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	});
 </script>
 
-<Layout>
+<Layout onResize={() => layoutEditors()}>
 	{#snippet navbar()}
-    Navbar
-  {/snippet}
+		Navbar: {fileId}
+	{/snippet}
 
-  {#snippet mainPane()}
-    Main Pane: {fileId}
-  {/snippet}
+	{#snippet mainPane()}
+		<MonacoEditor />
+	{/snippet}
 
-  {#snippet inputPane()}
-    Input Pane
-  {/snippet}
+	{#snippet inputPane()}
+		<MonacoEditor />
+	{/snippet}
 
-  {#snippet outputPane()}
-    Output Pane
-  {/snippet}
+	{#snippet outputPane()}
+		<MonacoEditor />
+	{/snippet}
 </Layout>
