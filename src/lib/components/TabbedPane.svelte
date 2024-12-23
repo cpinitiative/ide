6d276b7c73363e05a0c19a4ba41338a52448b44f
Nathan Wang <nathan.r.wang@gmail.com>
2024-12-22 18:47:35 -0800

    input / output pane tabs

---
+++ src/lib/components/TabbedPane.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	let {
+		tabs,
+		activeTab = $bindable(),
+		children
+	}: {
+		/**
+		 * A mapping of tab value => tab label.
+		 */
+		tabs: { [key: string]: string };
+		activeTab: string;
+		children: any;
+	} = $props();
+</script>
+
+<div class="flex h-full flex-col">
+	<div class="bg-black h-6 flex">
+		{#each Object.entries(tabs) as [value, label]}
+			<button
+				class={(value === activeTab
+					? 'bg-[#1E1E1E] text-gray-200'
+					: 'text-gray-400 hover:bg-gray-800 hover:text-gray-300 active:bg-gray-800') +
+					' inline-block px-4 text-sm font-medium transition focus:outline-none'}
+				onclick={() => (activeTab = value)}
+			>
+				{label}
+			</button>
+		{/each}
+	</div>
+	<div class="flex-1">
+		{@render children()}
+	</div>
+</div>
