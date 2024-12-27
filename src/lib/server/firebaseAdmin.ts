de9056fb19f12ba6472e9ac549b28667fc66e2ea
Nathan Wang <nathan.r.wang@gmail.com>
2024-12-26 22:07:27 -0600

    link firebaseAdmin.ts with PUBLIC_USE_FIREBASE_EMULATORS

--- src/lib/server/firebaseAdmin.ts
+++ src/lib/server/firebaseAdmin.ts
@@ -1,15 +1,20 @@
+import { PUBLIC_USE_FIREBASE_EMULATORS } from '$env/static/public';
 import { getApp, getApps, initializeApp, cert } from 'firebase-admin/app';
 
 if (getApps().length === 0) {
-	if (process.env.NEW_NEXT_PUBLIC_FIREBASE_PROJECT_ID) {
-		initializeApp({
-			credential: cert({
-				projectId: process.env.NEW_NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-				privateKey: process.env.NEW_FIREBASE_PRIVATE_KEY,
-				clientEmail: process.env.NEW_FIREBASE_CLIENT_EMAIL
-			}),
-			databaseURL: 'https://cp-ide-2-default-rtdb.firebaseio.com'
-		});
+	if (PUBLIC_USE_FIREBASE_EMULATORS !== 'true') {
+		if (!process.env.NEW_NEXT_PUBLIC_FIREBASE_PROJECT_ID) {
+			throw new Error('Missing required firebase environment variables');
+		} else {
+			initializeApp({
+				credential: cert({
+					projectId: process.env.NEW_NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+					privateKey: process.env.NEW_FIREBASE_PRIVATE_KEY,
+					clientEmail: process.env.NEW_FIREBASE_CLIENT_EMAIL
+				}),
+				databaseURL: 'https://cp-ide-2-default-rtdb.firebaseio.com'
+			});
+		}
 	} else {
 		initializeApp({
 			projectId: 'cp-ide-2',
