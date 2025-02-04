8728195b104c857162b0076fa1de20d39c5d2cc6
Daniel Zhu <danielfangzhu@gmail.com>
2025-02-04 10:16:18 -0800

    fix settings css

--- src/lib/components/SettingsDialog/SettingsDialog.svelte
+++ src/lib/components/SettingsDialog/SettingsDialog.svelte
@@ -12,7 +12,7 @@
 	import TextField from './TextField.svelte';
 
 	const {
-		elements: { trigger, overlay, content, title, description, close, portalled },
+		elements: { overlay, content, title, close, portalled },
 		states: { open: meltUiOpen }
 	} = createDialog({
 		forceVisible: true
@@ -93,7 +93,7 @@
 					<h2 use:melt={$title} class="text-center text-lg leading-6 font-medium">Settings</h2>
 				</div>
 
-				<div class="border-b border-gray-200">
+				<div class="border-b border-gray-200 dark:border-gray-700">
 					<nav class="-mb-px flex">
 						{@render SettingsTab(
 							'Workspace',
@@ -203,7 +203,7 @@
 					<div class="mt-6 flex items-center space-x-4">
 						<button
 							type="button"
-							class="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-none"
+							class="inline-flex items-center rounded-md border border-gray-300 bg-transparent px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-none dark:border-gray-600 dark:text-white dark:hover:bg-gray-700"
 							use:melt={$close}
 						>
 							Cancel
@@ -242,8 +242,8 @@
 	<button
 		class={(selected
 			? 'border-indigo-500 text-indigo-600'
-			: 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700') +
-			' group flex w-1/2 cursor-pointer items-center justify-center border-b-2 px-1 py-3 text-sm font-medium focus:outline-none'}
+			: 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:hover:border-gray-700 dark:hover:text-neutral-200') +
+			' group flex w-1/2 cursor-pointer items-center justify-center border-b-2 px-1 py-3 text-sm font-medium focus:outline-none dark:text-neutral-300'}
 		onclick={onClick}
 	>
 		<Icon
